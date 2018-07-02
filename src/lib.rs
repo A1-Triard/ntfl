d5a6f33e666618bf95ed5d73efc9f11950f00dd9
A1 <internalmike@gmail.com>
2018-07-02 03:08:26 +0300

    visual_type

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,29 @@ pub mod fw;
 pub mod inst;
 
 use std::sync::Arc;
-use fw::{ ValType, ValTypeDesc, Fw, Val };
+use fw::{ ValType, ValTypeDesc, Fw, Val, DepType, Type, DepProp, Obj };
+use window::Rect;
+
+pub struct Size {
+    height: isize,
+    width: isize,
+}
+
+impl Size {
+    pub fn new(height: isize, width: isize) -> Size {
+        if height < 0 || width < 0 { panic!("Size must be non-negative."); }
+        Size { height: height, width: width }
+    }
+    pub fn height(&self) -> isize { self.height }
+    pub fn width(&self) -> isize { self.width }
+}
 
 pub struct Ntfl<I> {
     str_type: ValType<I>,
     bool_type: ValType<I>,
+    rect_type: ValType<I>,
+    visual_type: DepType<I>,
+    visual_bounds_prop: DepProp<I>,
 }
 
 struct StrTypeDesc { }
@@ -50,15 +68,65 @@ impl<I> ValTypeDesc<I> for BoolTypeDesc {
     }
 }
 
-impl<I> Ntfl<I> {
-    pub fn new(fw: &mut Fw<I>) -> Ntfl<I> {
-        Ntfl {
-            str_type: fw.reg_val_type(Box::new(StrTypeDesc { })),
-            bool_type: fw.reg_val_type(Box::new(BoolTypeDesc { })),
+struct RectTypeDesc { }
+impl<I> ValTypeDesc<I> for RectTypeDesc {
+    fn name(&self) -> &str { &"Rect" }
+    fn parse(&self, type_: ValType<I>, s: &str) -> Option<Arc<Val<I>>> {
+        let s = s.trim();
+        if s.is_empty() { return Some(type_.box_(Rect::empty())); }
+        let mut parts = s.split(',');
+        if let Some(part1) = parts.next() {
+            if let Some(part2) = parts.next() {
+                if let Some(part3) = parts.next() {
+                    if let Some(part4) = parts.next() {
+                        if parts.next().is_some() { return None; }
+                        if let Ok(t) = part1.trim().parse::<isize>() {
+                            if let Ok(l) = part2.trim().parse::<isize>() {
+                                if let Ok(h) = part3.trim().parse::<isize>() {
+                                    if let Ok(w) = part4.trim().parse::<isize>() {
+                                        if h <= 0 || w <= 0 { return None; }
+                                        return Some(type_.box_(Rect::tlhw(t, l, h, w)));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        None
+    }
+    fn to_string(&self, val: &Val<I>) -> String {
+        let val = val.unbox::<Rect>();
+        if let Some((y, x)) = val.loc() {
+            let (h, w) = val.size();
+            format!("{},{},{},{}", y, x, h, w)
+        } else {
+            String::new()
         }
     }
-    pub fn str_type(&self) -> &ValType<I> { &self.str_type }
-    pub fn bool_type(&self) -> &ValType<I> { &self.bool_type }
+}
+
+impl<I> Ntfl<I> {
+    pub fn new(fw: &mut Fw<I>) -> Ntfl<I> {
+        let str_type = fw.reg_val_type(Box::new(StrTypeDesc { }));
+        let bool_type = fw.reg_val_type(Box::new(BoolTypeDesc { }));
+        let rect_type = fw.reg_val_type(Box::new(RectTypeDesc { }));
+        let visual_type = fw.reg_dep_type(String::from("Visual"), None);
+        let visual_bounds_prop = fw.reg_dep_prop(visual_type, String::from("Bounds"), Type::Val(rect_type), Obj::Val(rect_type.box_(Rect::empty())), None);
+        Ntfl {
+            str_type: str_type,
+            bool_type: bool_type,
+            rect_type: rect_type,
+            visual_type: visual_type,
+            visual_bounds_prop: visual_bounds_prop,
+        }
+    }
+    pub fn str_type(&self) -> ValType<I> { self.str_type }
+    pub fn bool_type(&self) -> ValType<I> { self.bool_type }
+    pub fn rect_type(&self) -> ValType<I> { self.rect_type }
+    pub fn visual_type(&self) -> DepType<I> { self.visual_type }
+    pub fn visual_bounds_prop(&self) -> DepProp<I> { self.visual_bounds_prop }
 }
 
 #[cfg(test)]
